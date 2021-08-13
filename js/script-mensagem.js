5abc409ae2589baf59e4f52c0dfc9b5c04fd622c
marcoshmarcarini <77796901+vainabiolink@users.noreply.github.com>
2021-08-13 16:56:01 -0300

    simple update

---
+++ js/script-mensagem.js
@@ -0,0 +1,10 @@
+let mensagem = document.querySelector(".mensagem")
+
+//mostra a mensagem
+function showMensagem(){
+    mensagem.style.display = "block"
+}
+// esconde a mensagem
+function hideMensagem(){
+    mensagem.style.display = "none"
+}
